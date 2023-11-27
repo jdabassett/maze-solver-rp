43451d41c8a5ed9cd4cd54ad6c6d18c887dab6ae
jdabassett <jacobbassett@gmail.com>
2023-11-27 11:38:10 -0700

    finished commenting on maze

--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -43,28 +43,54 @@ class Maze:
 
     @cached_property
     def width(self):
+        """
+        Returns max width of maze
+        :return: integer
+        """
         return max(square.column for square in self) + 1
 
     @cached_property
     def height(self):
+        """
+        Returns max height of maze
+        :return: integer
+        """
         return max(square.row for square in self) + 1
 
     @cached_property
     def entrance(self) -> Square:
+        """
+        Returns entrance from maze
+        :return: Square
+        """
         return next(sq for sq in self if sq.role is Role.ENTRANCE)
 
     @cached_property
     def exit(self) -> Square:
+        """
+        Returns exit from maze
+        :return: Square
+        """
         return next(sq for sq in self if sq.role is Role.EXIT)
 
 
 def validate_indices(maze: Maze) -> None:
+    """
+    Raises assertion if any square index from maze is not valid or in proper order
+    :param maze:
+    :return: None
+    """
     assert [square.index for square in maze] == list(
         range(len(maze.squares))
     ), "Wrong square.index"
 
 
 def validate_rows_columns(maze: Maze) -> None:
+    """
+    Raises assertion if any square row or column values from maze are not valid or in proper oder
+    :param maze:
+    :return: None
+    """
     for y in range(maze.height):
         for x in range(maze.width):
             square = maze[y * maze.width + x]
@@ -73,12 +99,22 @@ def validate_rows_columns(maze: Maze) -> None:
 
 
 def validate_entrance(maze: Maze) -> None:
+    """
+    Raises assertion if maze doesn't have exactly one entrance
+    :param maze:
+    :return: None
+    """
     assert 1 == sum(
         1 for square in maze if square.role is Role.ENTRANCE
     ), "Must be exactly one entrance"
 
 
 def validate_exit(maze: Maze) -> None:
+    """
+    Raises assertion if maze doesn't have exactly one exit
+    :param maze:
+    :return: None
+    """
     assert 1 == sum(
         1 for square in maze if square.role is Role.EXIT
     ), "Must be exactly one exit"
